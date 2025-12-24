6055d4eb5168d2d1e4b994ddd90e697c65a5f73c
PRIYANSHU <priyank2657@gmail.com>
2025-12-24 08:36:38 +0530

    added another page(js)

---
+++ scriptreservation.js
@@ -0,0 +1,30 @@
+let all7=document.getElementById("menu-section");
+let all8=document.getElementById("home-section");
+let all9=document.getElementById("blog-section");
+let all10=document.getElementById("reservation-section");
+let all11=document.getElementById("pages-section");
+
+all7.addEventListener("click", function () {
+  window.location.href = "menu.php";
+});
+all8.addEventListener("click", function () {
+  window.location.href = "index.php";
+});
+all9.addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+all10.addEventListener("click", function () {
+  window.location.href = "reservation.php";
+});
+all11.addEventListener("click", function () {
+  window.location.href = "pages.php";
+});
+let gotocart1=document.getElementById("ccart");
+let gotocart=document.getElementById("ccart1");
+
+gotocart1.addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
+gotocart.addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
