a951725ada0ec8322c4cad7f125565f6e68feee5
PRIYANSHU <priyank2657@gmail.com>
2025-12-24 08:40:08 +0530

    completed

---
+++ script.js
@@ -0,0 +1,93 @@
+let all55 = document.getElementById("all-navbar");
+let all1=document.getElementById("nav-burger");
+let all2=document.getElementById("nav-drinks");
+let all3=document.getElementById("nav-pasta");
+let all4=document.getElementById("nav-salads");
+let all5=document.getElementById("nav-deserts");
+let all6=document.getElementById("nav-pizzas");
+let all7=document.getElementById("menu-section");
+let all8=document.getElementById("home-section");
+let all9=document.getElementById("blog-section");
+let all10=document.getElementById("reservation-section");
+let all11=document.getElementById("pages-section");
+let all14=document.getElementById("view");
+
+all55.addEventListener("click", function () {
+  window.location.href = "index.php";
+});
+all1.addEventListener("click", function () {
+  window.location.href = "Menu-Burger.php";
+});
+all2.addEventListener("click", function () {
+  window.location.href = "Menu-Drinks.php";
+});
+all3.addEventListener("click", function () {
+  window.location.href = "Menu-Pasta.php";
+});
+all4.addEventListener("click", function () {
+  window.location.href = "Menu-Salads.php";
+});
+all5.addEventListener("click", function () {
+  window.location.href = "Menu-Desert.php";
+});
+all6.addEventListener("click", function () {
+  window.location.href = "Menu-Pizza.php";
+});
+all7.addEventListener("click", function () {
+  window.location.href = "menu.php";
+});
+all8.addEventListener("click", function () {
+  window.location.href = "index.php";
+});
+all9.addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+all10.addEventListener("click", function () {
+  window.location.href = "reservation.php";
+});
+all11.addEventListener("click", function () {
+  window.location.href = "pages.php";
+});
+all14.addEventListener("click", function () {
+  window.location.href = "category-shop.html";
+});
+
+
+let img1 = document.getElementById("jsburger");
+let img2 = document.getElementById("jsdeserts");
+let img3 = document.getElementById("jsdcp");
+img1.addEventListener("click", function () {
+  window.location.href = "Menu-Burger.php";
+});
+img2.addEventListener("click", function () {
+  window.location.href = "Menu-Desert.html";
+});
+img3.addEventListener("click", function () {
+  window.location.href = "Menu-Pizza.html";
+});
+let buttonviewmore = document.getElementById("viewmore1");
+buttonviewmore.addEventListener("click", function () {
+  window.location.href = "menu.php";
+});
+
+let details = document.getElementById("crust");
+let details1 = document.getElementById("comparison");
+let details2 = document.getElementById("kids");
+details.addEventListener("click", function () {
+  window.location.href = "PIZZ0N_WHERE_CRUST_MEETS_CREATIVITY.html";
+});
+details1.addEventListener("click", function () {
+  window.location.href = "Possession_so_comparison_inquietu.html";
+});
+details2.addEventListener("click", function () {
+  window.location.href = "OFFERS_MADE_SPECIALLY_FOR_KIDS.html";
+});
+let gotocart1=document.getElementById("ccart");
+let gotocart=document.getElementById("ccart1");
+
+gotocart1.addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
+gotocart.addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
---
+++ scriptpages.js
@@ -0,0 +1,46 @@
+let all17=document.getElementById("menu-section");
+let all18=document.getElementById("home-section");
+let all19=document.getElementById("blog-section");
+let all110=document.getElementById("reservation-section");
+let all111=document.getElementById("pages-section");
+
+all17.addEventListener("click", function () {
+  window.location.href = "menu.php";
+});
+all18.addEventListener("click", function () {
+  window.location.href = "index.php";
+});
+all19.addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+all110.addEventListener("click", function () {
+  window.location.href = "reservation.php";
+});
+all111.addEventListener("click", function () {
+  window.location.href = "pages.php";
+});
+
+
+let all113=document.getElementById("meatsaladpage");
+let all114=document.getElementById("lemonjuicepage");
+let all115=document.getElementById("sollowburgerpage");
+all113.addEventListener("click", function () {
+  window.location.href = "meatsalad.html";
+});
+
+all114.addEventListener("click", function () {
+  window.location.href = "lemonjuice.html";
+});
+
+all115.addEventListener("click", function () {
+  window.location.href = "sollowburger.html";
+});
+let gotocart1=document.getElementById("ccart");
+let gotocart=document.getElementById("ccart1");
+
+gotocart1.addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
+gotocart.addEventListener("click", function () {
+  window.location.href = "cart.html";
+});
